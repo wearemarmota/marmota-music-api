d78e43ef83a266f0a25173a64a8946349d8a08a7
Josep Viciana <emmgfx@gmail.com>
2020-09-11 23:07:59 +0200

    Prepare songs table for some metadata

---
+++ database/migrations/2020_09_11_224637_add_songs_columns.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSongsColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('songs', function (Blueprint $table) {
+            $table->string('genre')->nullable();
+            $table->integer('duration')->unsigned()->default(0);
+            $table->integer('position')->unsigned()->default(0);
+            $table->integer('position_of')->unsigned()->default(0);
+            $table->integer('disk')->unsigned()->default(0);
+            $table->integer('disk_of')->unsigned()->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('songs', function (Blueprint $table) {
+            $table->dropColumn('genre');
+            $table->dropColumn('duration');
+            $table->dropColumn('position');
+            $table->dropColumn('position_of');
+            $table->dropColumn('disk');
+            $table->dropColumn('disk_of');
+        });
+    }
+}
