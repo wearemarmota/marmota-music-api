5efc66fc1cfc8d8779b2b2290716a2159e9ceb06
Josep Viciana <emmgfx@gmail.com>
2020-10-05 11:25:57 +0200

    Include album covers if they are present

--- app/Album.php
+++ app/Album.php
@@ -26,10 +26,31 @@ class Album extends Model
       'artist_id',
     ];
 
+    protected $appends = ['coverFiles'];
+
     // More info about this feature:
     // https://laravel.com/docs/5.0/eloquent#eager-loading
     public function Artist()
     {
       return $this->belongsTo('App\Artist','artist_id');
     }
+
+    // More info about this feature:
+    // https://stackoverflow.com/a/60879655/1378408
+    // https://laravel.com/docs/7.x/eloquent-serialization#appending-values-to-json
+    public function getCoverFilesAttribute()
+    {
+        $coversFolder = "/storage/app/public/covers/";
+
+        if(!file_exists(__DIR__."/..{$coversFolder}{$this->uuid}-original.jpg")){
+            return [];
+        }
+
+        return [
+            "original"  => env('APP_URL') . "{$coversFolder}{$this->uuid}-original.jpg",
+            "500"       => env('APP_URL') . "{$coversFolder}{$this->uuid}-500.jpg",
+            "100"       => env('APP_URL') . "{$coversFolder}{$this->uuid}-100.jpg",
+        ];
+    }
+
 }
