0af9b240ff7fae78442ca8547417254c85406a7b
Josep Viciana <emmgfx@gmail.com>
2020-10-07 21:39:49 +0200

    Add PATCH to CORS allowed metods

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -21,7 +21,7 @@ class Cors
     {
         $headers = [
             'Access-Control-Allow-Origin'      => '*',
-            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, PATCH, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',
             'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
